0a8d89e939ea3f9f9b88586c07662c40cef0135e
t-satwik <tallurisatvik800@gmail.com>
2022-12-06 00:43:52 +0530

    Fixed a small issue

--- gan-bakery/src/App.js
+++ gan-bakery/src/App.js
@@ -39,6 +39,7 @@ function App() {
     const newCartItems = [...cartItems];
     if (newCartItems[productID] <= 0) {
       alert('No items in cart');
+      return;
     }
     newCartItems[productID]--;
     setCartItems(newCartItems);
